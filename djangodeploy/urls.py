543f0b92271dada95d92e05b074a67dfc2d6825a
Datastudy3769 <noble3769@gmail.com>
2023-11-01 16:21:43 +0900

    초기 서버형태로 복구

--- djangodeploy/urls.py
+++ djangodeploy/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+#from .views import index
 
 urlpatterns = [
-    path("",index),
+    path('admin/', admin.site.urls),
 ]
